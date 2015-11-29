b4d26fa8fb693153de74916a839072dbb441007a
Norredinea <me@norredine-abouriche.com>
2015-11-29 01:10:41 -0500

    remove form validation folder

--- angularjs-form-validation/js/app.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('RegistrationController', ['$scope', function($scope) {
-
-  $scope.register = function() {
-    $scope.message = 'Bienvenue ' + $scope.user.firstname;
-  };
-
-}]);
