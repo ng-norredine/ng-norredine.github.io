1641b2b74505258d99e57c71a706ee701e5d97f5
Norredinea <me@norredine-abouriche.com>
2016-06-27 11:02:03 -0400

    importing Angularjs scripts

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var demoMasonry = angular.module('demoMasonry', ['angularGrid'])
+    .service('imageService',['$q','$http',function($q,$http){
+        this.loadImages = function(){
+            return $http.jsonp("https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK");
+        };
+    }]);
+    demoMasonry.controller('demo', ['$scope','imageService', 'angularGridInstance', function ($scope,imageService,angularGridInstance) {
+        imageService.loadImages().then(function(data){
+            data.data.items.forEach(function(obj){
+                var desc = obj.description,
+                    width = desc.match(/width="(.*?)"/)[1],
+                    height = desc.match(/height="(.*?)"/)[1];
+
+                obj.actualHeight  = height;
+                obj.actualWidth = width;
+            });
+            $scope.pics = data.data.items;
+
+        });;
+    }]);
+    angular.bootstrap(document.getElementById("form-validation"),['formApp']);
